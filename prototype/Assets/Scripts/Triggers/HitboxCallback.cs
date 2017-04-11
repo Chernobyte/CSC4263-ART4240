035be41be1b26ced62659b6f90e2520b20f0e65f
Miles Robicheaux <mrobi37@tigers.lsu.edu>
2017-04-11 18:37:22 -0500

    Fixed #1 & made ALOT of structural changes
    I modified code from my Digital Media Minor Capstone course project (EE
    <some number here>). Most of the changes had to do with the hierarchy of
    GameObjects & their interactions amongst one another as well as keeping
    players from damaging themselves each time they fire. Took me way longer
    than it should have.

---
+++ prototype/Assets/Scripts/Triggers/HitboxCallback.cs
@@ -0,0 +1,38 @@
+﻿
+using System;
+using UnityEngine;
+
+public class HitboxCallback : MonoBehaviour {
+
+    public PlayerController parentPlayer;
+
+    Action<Collider2D> enterFunctionToCall;
+    Action<Collider2D> exitFunctionToCall;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void Init(Action<Collider2D> enter, Action<Collider2D> exit, PlayerController parentPlayer)
+    {
+        enterFunctionToCall = enter;
+        exitFunctionToCall = exit;
+        this.parentPlayer = parentPlayer;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        enterFunctionToCall(collision);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        exitFunctionToCall(collision);
+    }
+}
